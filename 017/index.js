0a8865ea8683ca4b0c3be166f4e3c9d3d8203cba
Cuthbert Guerrero <cguerrero@zendesk.com>
2020-01-16 09:26:18 +0800

    done with problem 17

--- 017/index.js
+++ 017/index.js
@@ -1,4 +1,5 @@
 const unique = [
+  0,
   'one'.length,
   'two'.length,
   'three'.length,
@@ -21,38 +22,68 @@ const unique = [
 ];
 
 const tens = [
-  'twenty'.length,
-  'thirty'.length,
-  'forty'.length,
-  'fifty'.length,
-  'sixty'.length,
-  'seventy'.length,
-  'eighty'.length,
-  'ninety'.length
+  0,
+  0,
+  "twenty".length,
+  "thirty".length,
+  "forty".length,
+  "fifty".length,
+  "sixty".length,
+  "seventy".length,
+  "eighty".length,
+  "ninety".length
 ];
 
-function countTens(ten) {
-  return unique.slice(0, 9).reduce((total, item) => total += ten + item, 0);
+const hundred = 'hundred'.length;
+const and = 'and'.length;
+const thousand = 'thousand'.length;
+
+function getForDoubleDigit(digit) {
+  if (digit < 20) {
+    return unique[digit]
+  } else {
+    var o = digit % 10;
+    var t = Math.floor(digit / 10);
+    return tens[t] + unique[o];
+  }
 }
 
-function belowOneHundred() {
-  const uniqueLength = unique.reduce((total, item) => total += item, 0);
-  const tensLength = tens.reduce((total, item) => total += countTens(item), 0);
-  return uniqueLength + tensLength;
+function getForHundreds(digit) {
+  var h = Math.floor(digit / 100);
+  var dd = digit % 100;
+  var total = unique[h] + hundred;
+
+  if (dd !== 0) {
+    total += and + getForDoubleDigit(dd);
+  }
+
+  return total;
 }
 
-function hundred(num) {
-  const hundredLength = num + 'hundred'.length;
-  const andLength = 'and'.length;
-  let total = hundredLength;
-  total += ((hundredLength + andLength) * 99) + belowOneHundred();
+function getForThousands(digit) {
+  var t = Math.floor(digit / 1000);
+  var h = digit % 1000;
+  var total = unique[t] + thousand;
 
-  return total
+  if (h !== 0){
+    total += getForHundreds(h);
+  }
+
+  return total;
 }
 
+var i;
+var current = 0;
+var total = 0;
+for(i = 1; i <= 1000; i++) {
+  if (i < 100) {
+    current = getForDoubleDigit(i);
+  } else if (i < 1000) {
+    current = getForHundreds(i);
+  } else {
+    current = getForThousands(i);
+  }
+  total += current;
+}
 
-var thousand = 'one'.length + 'thousand'.length;
-var total = belowOneHundred();
-total += unique.slice(0, 9).reduce((total, item) => total += hundred(item), 0);
-total += thousand;
 console.log(total);
