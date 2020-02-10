3ae146e4938f57ae0dec222bd141cb173f62032f
Cuthbert Guerrero <cguerrero@zendesk.com>
2020-02-10 22:36:06 +0800

    done with 56

---
+++ 055/index.js
@@ -0,0 +1,39 @@
+function reverseString(s) {
+  return s.split('').reverse().join('')
+}
+
+function reverseNum(n) {
+  let nStr = n.toString()
+  return Number(reverseString(nStr))
+}
+
+function isPalindome(i) {
+  let iStr = i.toString()
+  return iStr === reverseString(iStr)
+}
+
+function isLycrel(n) {
+  let i = 0;
+  let num = n
+  for (; i < 50; i++) {
+    num += reverseNum(num)
+    if (isPalindome(num)) { return false }
+  }
+
+  return true
+}
+
+function solution() {
+  let i
+  let total = 0
+  for (i = 1; i < 10000; i++) {
+    if (isLycrel(i)) {
+      total++
+    }
+  }
+  console.log(total)
+}
+
+console.time('solution')
+solution()
+console.timeEnd('solution')
